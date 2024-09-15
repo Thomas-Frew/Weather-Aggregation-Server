264e73bfc13fee1e605de9c6044be0de7ca046f7
ThomasFrew <thomasfrew13@gmail.com>
2024-09-15 15:41:52 +0930

    localhost resolved at test runtime

--- src/weatheraggregation/test/TestHelpers.java
+++ src/weatheraggregation/test/TestHelpers.java
@@ -3,6 +3,8 @@ package weatheraggregation.test;
 import weatheraggregation.core.FileHelpers;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,7 +12,17 @@ import java.nio.file.StandardCopyOption;
 
 public class TestHelpers {
     public static final int PORT = 2567;
-    public static final String IP = "172.29.96.1";
+
+    // Resolve localhost IP at runtime
+    public static final String IP;
+    static {
+        try {
+            IP = InetAddress.getLocalHost().toString().split("/", 2)[1];
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static final String HOSTNAME = IP+":"+PORT;
     public static final String DIRECTORY = "src/weatheraggregation/test/";
     public static final String WEATHER_DATA_FILENAME = DIRECTORY + "test_weather_data.txt";
